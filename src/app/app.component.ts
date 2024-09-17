abbd5940ef499339c8705c78ed412c659b5bacbe
Bisham6 <83863306+Bisham6@users.noreply.github.com>
2024-09-17 12:41:44 +0530

    first changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
+  apiMessage: string = '';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getData().subscribe({
+      next: (res:any)=> this.apiMessage = res.message,
+      error: (e:any) => console.error(e)
+    });
+  }
 }
